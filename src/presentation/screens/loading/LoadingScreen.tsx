69c5538a95914cab13cca3b0869ace268eab2d71
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2024-10-07 12:56:51 -0300

    estructura base y navegacion

---
+++ src/presentation/screens/loading/LoadingScreen.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export const LoadingScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>LoadingScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
