d0e7522e2db5bfc5cd844d364687d06d2005810d
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2024-10-07 13:04:47 -0300

    configuracion de kitten ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import '../gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './presentation/navigation/StackNavigator';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider } from '@ui-kitten/components';
 
 export const App = () => {
   return (
+    <ApplicationProvider
+    {...eva}
+    theme={eva.light}
+    >
     <NavigationContainer>
       <StackNavigator/>
     </NavigationContainer>
+    </ApplicationProvider>
   );
 };
 
